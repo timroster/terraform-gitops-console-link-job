3b69a6266e87b0d99fe28d9e580528af8bcebab5
Sean Sundberg <seansund@us.ibm.com>
2021-08-14 22:22:11 -0500

    Replaces setup-binaries script with submodule (#12)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,22 +1,16 @@
 locals {
-  bin_dir = "${path.cwd}/bin"
+  bin_dir  = module.setup_clis.bin_dir
   layer = "services"
   yaml_dir = "${path.cwd}/.tmp/console-link-job"
   name = "console-link-job"
 }
 
-resource null_resource setup_binaries {
-  provisioner "local-exec" {
-    command = "${path.module}/scripts/setup-binaries.sh"
-
-    environment = {
-      BIN_DIR = local.bin_dir
-    }
-  }
+module setup_clis {
+  source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
 }
 
 module "service_account" {
-  source = "github.com/cloud-native-toolkit/terraform-gitops-service-account.git?ref=v1.3.0"
+  source = "github.com/cloud-native-toolkit/terraform-gitops-service-account.git?ref=v1.3.1"
 
   gitops_config = var.gitops_config
   git_credentials = var.git_credentials
@@ -26,7 +20,7 @@ module "service_account" {
 }
 
 module "rbac" {
-  source = "github.com/cloud-native-toolkit/terraform-gitops-rbac.git?ref=v1.6.0"
+  source = "github.com/cloud-native-toolkit/terraform-gitops-rbac.git?ref=v1.6.1"
 
   cluster_scope = true
 
@@ -61,8 +55,6 @@ module "rbac" {
 }
 
 resource null_resource create_yaml {
-  depends_on = [null_resource.setup_binaries]
-
   provisioner "local-exec" {
     command = "${path.module}/scripts/create-yaml.sh '${local.yaml_dir}' '${module.service_account.name}'"
   }
@@ -72,7 +64,7 @@ resource null_resource setup_gitops {
   depends_on = [null_resource.create_yaml]
 
   provisioner "local-exec" {
-    command = "$(command -v igc || command -v ${local.bin_dir}/igc) gitops-module '${local.name}' -n '${var.namespace}' --contentDir '${local.yaml_dir}' --serverName '${var.server_name}' -l '${local.layer}'"
+    command = "${local.bin_dir}/igc gitops-module '${local.name}' -n '${var.namespace}' --contentDir '${local.yaml_dir}' --serverName '${var.server_name}' -l '${local.layer}'"
 
     environment = {
       GIT_CREDENTIALS = yamlencode(var.git_credentials)
